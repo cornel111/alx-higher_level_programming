7594020b61134fad4dde8e51c23fcee6ed502fd9
Ogar Cornelius <corneliusogar.cl@gmail.com>
2022-12-07 11:01:26 -0800

    task 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
+
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
