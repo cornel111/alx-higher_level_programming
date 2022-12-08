8bae166d06fd40c85bf11a1a0c25e3130e3cc914
Ogar Cornelius <corneliusogar.cl@gmail.com>
2022-12-07 20:57:02 -0800

    uncheched tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
-
 def best_score(a_dictionary):
-    """
-    A function that returns a key with the biggest integer value.
-    """
-    if a_dictionary:
-        my_list = list(a_dictionary.keys())
-        score = 0
-        leader = ""
-        for i in my_list:
-            if a_dictionary[i] > score:
-                score = a_dictionary[i]
-                leader = i
-        return leader 
+    if a_dictionary is not None:
+        score = None
+        person = None
+        for k in a_dictionary.keys():
+            if score is None or a_dictionary[k] > score:
+                score = a_dictionary[k]
+                person = k
+        return person 
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,5 @@
-Bytes
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    """
-    A function that returns a set of
-    common elements in two sets
-    """
-    return(set_1 & set_2)
+    if set_1 is not None and set_2 is not None:
+        return set_1 & set_2
