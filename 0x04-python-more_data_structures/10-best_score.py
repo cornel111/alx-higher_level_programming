68eea3851cb4ca45b43e94a6bb43eba99e4c2764
Ogar Cornelius <corneliusogar.cl@gmail.com>
2022-12-09 11:23:10 -0800

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-# 10-best_score.py
-
-
 def best_score(a_dictionary):
-    """return a key with big int value."""
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
+    if not a_dictionary:
+        return (None)
 
-    lst = list(a_dictionary.keys())[0]
-    bg = a_dictionary[lst]
-    for j, k in a_dictionary.items():
-        if v > bg:
-            bg = j
-            lst = j
-    return (lst)
+    return (max(a_dictionary, key=a_dictionary.get))
